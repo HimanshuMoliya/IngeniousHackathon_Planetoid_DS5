54a4bcec226ae8b6b96da6b4f6aa5631852a407d
Himanshu Moliya <33668957+HimanshuMoliya@users.noreply.github.com>
2018-03-03 18:17:46 +0530

    Add files via upload

---
+++ resto.py
@@ -0,0 +1,22 @@
+# Himanshu Moliya
+# hackthon : Aadhar card data minimization
+# this file is use for restor data from image
+import cv2
+
+imagename = input("Enter name of Encoded image file ?(.bmp or .png) ")
+# Try to restore the message from image.
+img = cv2.imread(imagename, cv2.IMREAD_GRAYSCALE)
+i = 0
+bits = ''
+chars = []
+for row in img:
+    for pixel in row:
+        bits = str(pixel & 0x01) + bits
+        i += 1
+        if(i == 8):
+            chars.append(chr(int(bits, 2)))
+            i = 0
+            bits = ''
+print(''.join(chars))
+
+
