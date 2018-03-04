c20e73d1f878366552f4442c16766e3a8b6ec328
Himanshu Moliya <33668957+HimanshuMoliya@users.noreply.github.com>
2018-03-04 14:13:05 +0530

    Add files via upload

---
+++ Demo1Minimization/demo_restore.sh
@@ -0,0 +1,4 @@
+python2 planetoid.py -d him.bmp
+python2 planetoid.py -d outbapa.png
+python2 planetoid.py -d rah.jpg
+
---
+++ Demo1Minimization/demo_store.sh
@@ -0,0 +1,3 @@
+python2 planetoid.py -e Old_Data/rahul.jpg rah.jpg Old_Data/rahul.txt
+python2 planetoid.py -e Old_Data/himanshu.bmp him.bmp Old_Data/himanshu.txt
+python2 planetoid.py -e Old_Data/bapa.png outbapa.png Old_Data/bapa.txt
